000951d28dd45d015c5ea8f8fe0dcb7789613d1b
Mohammed Elgaily <mbajily@gmail.com>
2023-03-30 03:25:47 +0200

    Add 'entrypoint.sh' file.

--- django_k8s/settings.py
+++ django_k8s/settings.py
@@ -110,7 +110,6 @@ if DB_IS_AVAILABLE and POSTGRES_READY:
         }
     }
 
-print(DATABASES)
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
